aadefd4a5e84ee747c97daa2ccd59eaa5c752017
Tomo Tomov <48110573+tomotomov92@users.noreply.github.com>
2024-11-06 13:13:31 +0200

    Update Server.sh

--- Server.sh
+++ Server.sh
@@ -41,7 +41,7 @@ echo 'net.ipv4.ip_forward = 1' | sudo tee -a /etc/sysctl.d/99-tailscale.conf
 echo 'net.ipv6.conf.all.forwarding = 1' | sudo tee -a /etc/sysctl.d/99-tailscale.conf
 sudo sysctl -p /etc/sysctl.d/99-tailscale.conf
 sudo tailscale set --advertise-exit-node
-sudo tailscale up --accept-routes --advertise-exit-node --advertise-routes=192.168.2.0/24,192.168.1.0/24 --ssh
+sudo tailscale up --advertise-exit-node --advertise-routes=192.168.2.0/24,192.168.1.0/24 --ssh
 
 ## Install Openssh-server
 sudo apt install openssh-server
